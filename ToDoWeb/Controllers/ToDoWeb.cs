2e49e15719d8a5904b4ef946ab2be175ed4d9676
shubhamdhamane <A00257743@mycambrian.ca>
2022-08-21 00:58:22 -0400

    errors solve

--- ToDoWeb/Controllers/ToDoWeb.cs
+++ ToDoWeb/Controllers/ToDoWeb.cs
@@ -9,8 +9,10 @@ using ToDoWeb.Models;
 
 namespace ToDoWeb.Controllers
 {
+    
     public class ToDoWeb : Controller
     {
+        public int i = 0;
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -19,5 +21,21 @@ namespace ToDoWeb.Controllers
 
             return View("Index", toDo);
         }
+    }
+
+    [HttpPost]
+    public IActionResult Index(Models.ToDoWeb ToDoList, String submit, string completed)
+    {
+        if (submit == "submit")
+        {
+            ToDoList.id = "1";
+            ToDoList.title = "name";
+            ToDoList.completedDate = "NA";
+            ToDoList.isCompleted = "NA";
+        }
+        else if (completed == "completed")
+        {
+        }
+        return View();
     }
 }
